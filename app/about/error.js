7894a701e631c5a97fe5b64752da60db968b0718
shoebHamim <hamim99bd@gmail.com>
2023-02-03 18:40:39 +0600

    first commit

---
+++ app/about/error.js
@@ -0,0 +1,9 @@
+"use client"
+export default function Error({error,reset}){
+  return (
+    <div>
+
+    </div>
+  )
+
+}
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,10 @@
 import './globals.css'
-
+import { Montserrat } from '@next/font/google'
+const montserrat=Montserrat({
+  weight:['400','700'],
+  subsets:['latin'],
+  variable:'--font-montserrat'
+})
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -8,7 +13,16 @@ export default function RootLayout({ children }) {
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className={`${montserrat.className}`}>
+        <nav   >
+          <ul className='flex gap-6'>
+          <h2><a href="/">Logo</a></h2>
+            <li><a href="./about">About</a></li>
+            <li><a href="">Sign Up</a></li>
+          </ul>
+        </nav>
+        
+        {children}</body>
     </html>
   )
 }
